13a3558f7d3748a68857d5276968ce602c8af74f
ibrsamwel <ibraah568@gmail.com>
2026-03-20 22:05:10 +0300

    Add POST route to allow adding new inventory

--- main.go
+++ main.go
@@ -3,12 +3,9 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
-	
-	
 	"github.com/ibraah007/hospital-inventory-api/models" 
 )
 
-/
 var inventory = []models.Item{
 	{ID: "1", Name: "Paracetamol", Quantity: 500},
 	{ID: "2", Name: "Surgical Masks", Quantity: 2000},
@@ -17,12 +14,24 @@ var inventory = []models.Item{
 func main() {
 	r := gin.Default()
 
-	r.GET("/health", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"status": "Hospital API is Online"})
-	})
-
+	// 1. View all items
 	r.GET("/inventory", func(c *gin.Context) {
 		c.JSON(http.StatusOK, inventory)
+	})
+
+	// 2. Add a new item (The Loading Dock)
+	r.POST("/inventory", func(c *gin.Context) {
+		var newItem models.Item
+
+		// Call BindJSON to bind the received JSON to newItem
+		if err := c.ShouldBindJSON(&newItem); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// Add the new item to the slice
+		inventory = append(inventory, newItem)
+		c.JSON(http.StatusCreated, newItem)
 	})
 
 	r.Run(":8080")
