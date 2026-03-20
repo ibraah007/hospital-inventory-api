820512803e2c467ed9228637558dc98bb5570f6b
ibrsamwel <ibraah568@gmail.com>
2026-03-20 21:43:43 +0300

    Refactor: Move Item struct to separate models package

--- main.go
+++ main.go
@@ -3,16 +3,26 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	
+	// This points to the folder you just created
+	"github.com/ibraah007/hospital-inventory-api/models" 
 )
+
+// Now we use models.Item instead of just Item
+var inventory = []models.Item{
+	{ID: "1", Name: "Paracetamol", Quantity: 500},
+	{ID: "2", Name: "Surgical Masks", Quantity: 2000},
+}
 
 func main() {
 	r := gin.Default()
 
-	// This uses the "net/http" package (http.StatusOK)
 	r.GET("/health", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"status": "Hospital API is Online",
-		})
+		c.JSON(http.StatusOK, gin.H{"status": "Hospital API is Online"})
+	})
+
+	r.GET("/inventory", func(c *gin.Context) {
+		c.JSON(http.StatusOK, inventory)
 	})
 
 	r.Run(":8080")
